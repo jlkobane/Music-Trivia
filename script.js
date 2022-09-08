305e049ea3b52e2f44124a180daa3b40cff7251f
jlkobane <jaysonkobane88@gmail.com>
2022-09-08 12:52:55 -0700

    Add more html

--- script.js
+++ script.js
@@ -1,3 +1,19 @@
-const rapLinkEl = document.querySelector('.Rap-Link');
+const homeScreenEl = document.querySelector('.Home-screen')
 
-rapLinkEl.addEventListener('click');
+const rapButtonEl = document.querySelector('.Rap-Link') 
+
+const gameSelectorbuttonEl = document.querySelector('.Game-selectors') 
+
+const gameSubjectsEl = document.querySelector('.Game-subjects-rap') 
+
+rapButtonEl.addEventListener('click', function () { 
+
+    homeScreenEl.style.visibility = 'hidden';
+
+}); 
+
+gameSelectorbuttonEl.addEventListener('click', function () { 
+
+    gameSubjectsEl.style.visibility = 'hidden'
+
+});
