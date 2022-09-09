555351ca57cb126f72ab27de069912f7c52f794e
jlkobane <jaysonkobane88@gmail.com>
2022-09-08 17:23:01 -0700

    Fixed button function

--- script.js
+++ script.js
@@ -4,16 +4,33 @@ const rapButtonEl = document.querySelector('.Rap-Link')
 
 const gameSelectorbuttonEl = document.querySelector('.Game-selectors') 
 
-const gameSubjectsEl = document.querySelector('.Game-subjects-rap') 
+const gameSubjectsEl = document.querySelector('.Game-subjects-rap')  
+
+const questionsEl = document.querySelector('.Emo-Rap-Questions');  
+
+
+
+ gameSubjectsEl.style.display = 'none';
+ questionsEl.style.display = 'none';
 
 rapButtonEl.addEventListener('click', function () { 
 
-    homeScreenEl.style.visibility = 'hidden';
+    homeScreenEl.style.display = 'none'; 
+    gameSubjectsEl.style.display = 'inline'; 
+
 
 }); 
 
 gameSelectorbuttonEl.addEventListener('click', function () { 
 
-    gameSubjectsEl.style.visibility = 'hidden'
+   gameSubjectsEl.style.display = 'none'; 
+   questionsEl.style.display = 'inline'; 
 
-});
+
+}); 
+
+function userInput() { 
+  
+
+
+}
