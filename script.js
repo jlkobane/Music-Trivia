cb288eb99a800fc4d226515b8951c1b5c102855a
jlkobane <jaysonkobane88@gmail.com>
2022-09-10 11:45:36 -0700

    Add images and text styling

--- script.js
+++ script.js
@@ -8,22 +8,62 @@ const gameSubjectsEl = document.querySelector('.Game-subjects-rap')
 
 const questionsEl = document.querySelector('.Emo-Rap-Questions');  
 
-const answerInputEl = document.querySelector('#User-Answer');   
+const answerInput1El = document.querySelector('#User-Answer1');   
+
+const answerInput2El = document.querySelector('#User-Answer2');   
+
+const answerInput3El = document.querySelector('#User-Answer3');   
+
+const answerInput4El = document.querySelector('#User-Answer4');   
+
+const answerInput5El = document.querySelector('#User-Answer5');   
+
+const answerInput6El = document.querySelector('#User-Answer6');   
+
+const answerInput7El = document.querySelector('#User-Answer7');   
+
+const answerInput8El = document.
+querySelector('#User-Answer8');   
+
+const answerInput9El = document.querySelector('#User-Answer9');   
+
+const answerInput10El = document.querySelector('#User-Answer10');   
 
 const getResultsEl = document.querySelector('#Score-Result')
 
 const counterEl = document.querySelector('#Score-Counter') 
 
-const counterDisplay = document.querySelector('.Counter-Display')
+const counterDisplay = document.querySelector('.Counter-Display') 
+
+const hereButtonEl = document.querySelector('.here-button');
 
 let scoreCounter = 0 
 
-let emoRapAnswers = ['2019']
+let emoRapAnswers = [
+	'2019',
+	'mid-2010s',
+	'Yung Lean',
+	'2013',
+	'Jahseh Dwayne Ricardo Onfroy',
+	"5'4",
+	'Twice',
+	'Lucid Dreams',
+	'March 16th 2018',
+	'Michael Lamar White',
+];
 
 gameSubjectsEl.style.display = 'none';
 questionsEl.style.display = 'none';
 
 rapButtonEl.addEventListener('click', function () { 
+    
+    homeScreenEl.style.display = 'none'; 
+    gameSubjectsEl.style.display = 'inline'; 
+    
+    
+}); 
+
+hereButtonEl.addEventListener('click', function () { 
     
     homeScreenEl.style.display = 'none'; 
     gameSubjectsEl.style.display = 'inline'; 
@@ -42,7 +82,43 @@ gameSelectorbuttonEl.addEventListener('click', function () {
 
 
 function userInput() { 
-    if (answerInputEl.value == emoRapAnswers[0]) { 
+    if (answerInput1El.value == emoRapAnswers[0]) { 
+        scoreCounter += 1
+        
+    }
+    if (answerInput2El.value == emoRapAnswers[1]) { 
+        scoreCounter += 1
+        
+    }
+    if (answerInput3El.value == emoRapAnswers[2]) { 
+        scoreCounter += 1
+        
+    }
+    if (answerInput4El.value == emoRapAnswers[3]) { 
+        scoreCounter += 1
+        
+    }
+    if (answerInput5El.value == emoRapAnswers[4]) { 
+        scoreCounter += 1
+        
+    }
+    if (answerInput6El.value == emoRapAnswers[5]) { 
+        scoreCounter += 1
+        
+    }
+    if (answerInput7El.value == emoRapAnswers[6]) { 
+        scoreCounter += 1
+        
+    }
+    if (answerInput8El.value == emoRapAnswers[7]) { 
+        scoreCounter += 1
+        
+    }
+    if (answerInput9El.value == emoRapAnswers[8]) { 
+        scoreCounter += 1
+        
+    }
+    if (answerInput10El.value == emoRapAnswers[9]) { 
         scoreCounter += 1
         
     }
@@ -56,7 +132,7 @@ getResultsEl.addEventListener('click', function () {
     counterEl.style.display = 'inline';
     counterDisplay.style.display = 'inline'
     let percentage = scoreCounter * 10 
-    counterEl.innerHTML = `${scoreCounter}/10! ${percentage}%` 
+    counterEl.innerHTML = `${scoreCounter}/10 ${percentage}%` 
     console.log(scoreCounter)
 
 }); 
